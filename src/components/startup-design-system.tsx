d1048a97b195164f672c95fc4a354cd33b749a53
acaldas <acaldas.dev@gmail.com>
2024-10-03 12:45:18 +0100

    feat: updated dependencies

--- src/components/startup-design-system.tsx
+++
@@ -1,7 +0,0 @@
-import {
-    ToastContainer,
-    UiNodesContextProvider,
-    WagmiContext,
-} from '@powerhousedao/design-system';
-
-export default { ToastContainer, UiNodesContextProvider, WagmiContext };
